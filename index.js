a27aca01454a8680d02808f74b679704889d908d
DOROTHY2322 <kimelidorothy@gmail.com>
2022-11-28 20:20:51 +0300

    Array function: Filter

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+//findMatching- This function takes an array of drivers' names and a string as arguments, and returns the matching list of drivers. The function should be case insensitive.
+function findMatching(drivers, attribute) {
+    return drivers.filter(function (driver) { return driver.toLowerCase() === attribute.toLowerCase() })
+  }
+// function takes an array of drivers' names and a string as arguments for querying the array, and returns all drivers whose names begin with the provided letters.
+  function fuzzyMatch(drivers, attribute) {
+    return drivers.filter(function (driver) { return driver.toLowerCase().substring(0, attribute.length) === attribute.toLowerCase() })
+  }
+//This function takes an array of driver objects and a string as arguments.
+  function matchName(drivers, argument) {
+    return drivers.filter(function (driver) { return driver.name === argument })
+  }
